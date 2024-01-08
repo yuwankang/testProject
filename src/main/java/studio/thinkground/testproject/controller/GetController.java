db571e822b1eaaf666412b8d0045c4838c593507
yuwankang <kangyuwan@naver.com>
2024-01-08 23:50:38 +0900

    Get API 만들기

---
+++ src/main/java/studio/thinkground/testproject/controller/GetController.java
@@ -0,0 +1,50 @@
+package studio.thinkground.testproject.controller;
+
+import org.springframework.web.bind.annotation.*;
+import studio.thinkground.testproject.dto.MemberDTO;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/get-api")
+public class GetController {
+
+    //http://localhost:8080/api/v1/get-api/hello
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String getHello(){
+        return "Hello World";
+    }
+    @GetMapping(value = "/name")
+    public String getName() {
+        return "Flature";
+    }
+    @GetMapping(value = "/variable1/{variable}")
+    public String getVariable1(@PathVariable String variable){
+        return variable;
+    }
+    @GetMapping(value = "/variable2/{variable}")
+    public String getVariable2(@PathVariable("variable") String var){
+        return var;
+    }
+    @GetMapping(value = "/request1")
+    public String getRequestParam1(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam String organization){
+        return name +" "+ email+" "+ organization;
+    }
+    @GetMapping(value = "/request2")
+    public String getRequestParam2(@RequestParam Map<String, String> param){
+        StringBuilder sb = new StringBuilder();
+
+        param.entrySet().forEach(map ->{
+           sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+        return sb.toString();
+    }
+
+    @GetMapping(value = "/request3")
+    public String getRequestParam3(MemberDTO memberDTO){
+        return memberDTO.toString();
+    }
+}
