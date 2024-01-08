60098048cff9a7ce55cb107886cd54ce61a09caf
yuwankang <kangyuwan@naver.com>
2024-01-08 23:04:27 +0900

    Hello world 출력하기

--- src/main/java/studio/thinkground/testproject/TestProjectApplication.java
+++ src/main/java/studio/thinkground/testproject/TestProjectApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(TestProjectApplication.class, args);
+    }
 
 }
--- src/main/java/studio/thinkground/testproject/controller/HelloController.java
+++ src/main/java/studio/thinkground/testproject/controller/HelloController.java
@@ -1,2 +1,13 @@
-package studio.thinkground.testproject.controller;public class HelloController {
+package studio.thinkground.testproject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/hello")
+    public String hello(){
+        return "Hello World";
+    }
 }
